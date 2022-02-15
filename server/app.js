527374e4760d17389ffb34b95d46b7477b0dfed5
aniOfri <46932810+aniOfri@users.noreply.github.com>
2022-02-15 18:00:55 +0200

    added background dataset to test and updated loading data function

--- server/app.js
+++ server/app.js
@@ -32,16 +32,15 @@ async function getData(){
     train_data = [];
     for (let label of LABELS){
         const files_names = await readdir(TRAINING+label);
-        for (let i = 0; i < 500; i++){
+        for (let i = 0; i <= 500; i++){
             let file_path = TRAINING+label+"/"+files_names[i];
-            PNG.decode(file_path, function(pixels) {
+            await PNG.decode(file_path, function(pixels) {
                 var pixels = Array.from(pixels);
                 var array = []
-                for (let i = 0; i < 90*70*4; i+=4){
-                    
-                    array.push(pixels[i+3]/255);
+                for (let j = 0; j < 90*70*4; j+=4){
+                    array.push(pixels[j+3]/255);
                 }
-                train_data.push[label, array];
+                train_data.push([label, array]);
             });
         }
     }
@@ -50,16 +49,15 @@ async function getData(){
     test_data = [];
     for (let label of LABELS){
         const files_names = await readdir(TEST+label);
-        for (let i = 0; i < 500; i++){
+        for (let i = 0; i < 350; i++){
             let file_path = TEST+label+"/"+files_names[i];
             PNG.decode(file_path, function(pixels) {
                 var pixels = Array.from(pixels);
                 var array = []
-                for (let i = 0; i < 90*70*4; i+=4){
-                    
+                for (let j = 0; j < 90*70*4; j+=4){
                     array.push(pixels[i+3]/255);
                 }
-                train_data.push[label, array];
+                test_data.push([label, array]);
             });
         }
     }
@@ -68,10 +66,23 @@ async function getData(){
     return [train_data, test_data];
 }
 
-function shuffleArray(array){
-    const shuffledArray = array.sort((a, b) => 0.5 - Math.random());
-    return shuffledArray;
-}
+function shuffleArray(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
 
 function getModel(){
 
@@ -92,6 +103,7 @@ async function doPrediction(model, data, batchSize = 500) {
 async function start(){
     const data = await getData();
 
+    console.log(data);
     //model = getModel();
     
     //await train(model, data);
