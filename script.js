bf99cb09efd9d5318a03eb8efab4ed7bf8241b4f
aniOfri <ofri0608@gmail.com>
2022-02-17 15:57:11 +0200

    small updates on client side

--- script.js
+++ script.js
@@ -4,6 +4,29 @@ $(document).ready(function () {
     });
  });
 
+ var video = document.querySelector("#videoElement");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+       console.log(stream     )
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+}
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+function intervalTimer() {
+   ctx.clearRect(0,0,400,400);
+   ctx.drawImage(video,0,0,400,400);
+ 
+ }
+ var myVar = setInterval(intervalTimer, 1000);
+
 
  function sendCanvas(){
       $.post("http://localhost:3000/request",
@@ -14,5 +37,5 @@ $(document).ready(function () {
       });
   }
  
-  setInterval(sendCanvas, 1000);
+  //setInterval(sendCanvas, 1000);
  
